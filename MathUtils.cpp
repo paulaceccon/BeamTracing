c1370ebdcf1d02c2502d39ffae8a43018ac48d07
Paula Ceccon <pceccon@172-16-30-180.DYNAPOOL.NYU.EDU>
2015-10-29 12:50:17 -0400

    A collection of math methods.
    These methods are necessary to properly perform the sound propagation.

---
+++ MathUtils.cpp
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   MathUtils.cpp
+ * Author: pceccon
+ * 
+ * Created on October 29, 2015, 11:01 AM
+ */
+
+#include "MathUtils.h"
+
+void MathUtils::lineEquation(const core::Pointf& pa, const core::Pointf& pb, float a, float b, float c)
+{
+    a = pb.y - pa.y;
+    b = (pb.x - pa.x) * -1;
+    c = pb.x * pa.y - (pb.y * pa.x); 
+}
+
+void MathUtils::pointOfIntersection(const core::Pointf& p1a, const core::Pointf& p2a,
+       const core::Pointf& p1b, const core::Pointf& p2b, core::Pointf& out) 
+{
+    float a1 = p1a.y - p1b.y;
+    float b1 = (p1a.x - p1b.x) * -1;
+    float c1 = p1a.x * p1b.y - (p1a.y * p1b.x);
+        
+    float a2 = p2a.y - p2b.y;
+    float b2 = (p2a.x - p2b.x) * -1;
+    float c2 = p2a.x * p2b.y - (p2a.y * p2b.x);
+        
+    float u = b1 * c2 - (c1 * b2);
+    float v = (a1 * c2 - (c1 * a2)) * -1;
+    float w = a1 * b2 - (b1 * a2);
+    
+    if (w != 0)
+    {
+        out.x = u/w;
+        out.y = v/w;
+    }
+}
---
+++ MathUtils.h
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   MathUtils.h
+ * Author: pceccon
+ *
+ * Created on October 29, 2015, 11:01 AM
+ */
+
+#ifndef MATHUTILS_H
+#define MATHUTILS_H
+
+#include "Core.h"
+
+class MathUtils 
+{
+    
+    public:
+
+        /**
+        * Find the line equation (@a x + @b y + @c) 
+        * given to points, @pa and @pb, on that line.
+        * 
+        * @param pa First point on the line  (start point).
+        * @param pb Second point on the line (end point).
+        * @param a  Value @a that defines the equation of a line. 
+        * @param b  Value @b that defines the equation of a line.
+        * @param c  Value @c that defines the equation of a line.
+        */
+       static void lineEquation(const core::Pointf& pa, const core::Pointf& pb, float a, float b, float c);
+       
+       /**
+        * Define the point of intersection between two lines, if it exists.
+        * 
+        * @param p1a First point on the first line.
+        * @param p2a First point on the second line. 
+        * @param p1b Second point on the first line.
+        * @param p2b Second point on the second line.
+        * @param out A reference for the point of intersection to be written.
+        */
+       static void pointOfIntersection(const core::Pointf& p1a, const core::Pointf& p2a,
+        const core::Pointf& p1b, const core::Pointf& p2b, core::Pointf& out);
+
+};
+
+#endif /* MATHUTILS_H */
+
